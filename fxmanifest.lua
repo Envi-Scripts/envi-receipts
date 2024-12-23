b53ce24224354f82777d7a758f514f21c820b871
Sr.Asorey <braismasorey@gmail.com>
2024-12-23 17:16:39 +0100

    New version for updated features.
    New UI with scrollable interaction. Edited addBill export to use it with automatic bills on jobs without player interaction.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,20 @@
 
 fx_version 'cerulean'
 
+author 'Envi-Scripts'
+description 'Receipt system created by Envi-Scripts, redesigned and updated by Sr. Asorey'
+version '1.1.3'
 game 'gta5'
 lua54 'yes'
 
 ui_page 'html/index.html'
 
 files { 
-'html/index.html', 
-'html/index.css', 
-'html/index.js',
-'html/img/barcode.png',
+	'html/index.html',
+	'html/index.css',
+	'html/index.js',
+	'html/img/barcode.png',
+	'html/img/paperbg.jpg',
 }
 
 client_scripts {
@@ -26,6 +30,3 @@ server_scripts {
 }
 
 data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_prop_payment_terminal.ytyp'
-
--- version '1.1.2 - FULL COMPATIBILITY WHEN USING OX_INVENTORY WITH QB-CORE'
-
--- html/index.js
+++ html/index.js
@@ -1,85 +1,67 @@
-function updateReceiptHeight() {
-    const baseHeight = 300;
-    const itemHeight = 20;
-    const maxVisibleItems = 8;
-  
-    const items = document.querySelectorAll('.Receipt .item');
-    const itemCount = items.length;
-  
-    if (itemCount > maxVisibleItems) {
-      const extraItems = itemCount - maxVisibleItems;
-      const newHeight = baseHeight + (extraItems * itemHeight);
-      document.querySelector('.Receipt').style.height = `${newHeight}px`;
+window.addEventListener('message', function (event) {
+  if (event.data.type === "ui") {
+    if (event.data.status) {
+      updateUI(event.data.metadata);
+      display(true);
     } else {
-      document.querySelector('.Receipt').style.height = `${baseHeight}px`;
+      display(false);
     }
   }
-  
-  window.addEventListener('message', function(event) {
-    console.log("Received metadata:", event.data.metadata);
-    if (event.data.type === "ui") {
-        if (event.data.status) {
-            updateUI(event.data.metadata);
-            display(true);
-        } else {
-            display(false);
-        }
-    }
-  });
-  
-  function updateUI(metadata) {
-    console.log("Received metadata:", metadata);
-    $('.header div:nth-child(1)').text('Date: ' + metadata.date);
-    $('.header div:nth-child(2)').text('Time: ' + metadata.time);
+});
+
+function updateUI(metadata) {
+  $('.header div:nth-child(1)').text('Date: ' + metadata.date);
+  $('.header div:nth-child(2)').text('Hour: ' + metadata.time);
+  if (metadata.type && metadata.type !== null) {
     $('.business-name').text(metadata.type);
-    $('.item').remove();
-      for (var i = 1; metadata['item' + i] && metadata['price' + i]; i++) {
-        var itemDiv = $('<div class="item"></div>');
-        var descriptionDiv = $('<div class="description"></div>').text(metadata['item' + i]);
-        var priceDiv = $('<div class="price"></div>').text('$' + metadata['price' + i]);
-        itemDiv.append(descriptionDiv);
-        itemDiv.append(priceDiv);
-        $('.Receipt').append(itemDiv);
-    }
-    let totalCost = 0;
-    for (var i = 1; metadata['price' + i]; i++) {
-      totalCost += parseFloat(metadata['price' + i]);
-    }
-  
-    $('.sub-total').text('Sub Total: $' + totalCost.toFixed(2));
-    $('.tax-amount').text('Tax: $' + metadata.tax_amount);
-    $('.grand-total').text('Total: $' + metadata.total_after_tax);
-    updateReceiptHeight();
+  } else {
+    $('.business-name').text('');
   }
-  
-  function display(bool) {
-    if (bool) {
-        $('.Receipt').fadeIn(350);
+  $('.item').remove();
+  for (var i = 1; metadata['item' + i] && metadata['price' + i]; i++) {
+    var itemDiv = $('<div class="item"></div>');
+    var descriptionDiv = $('<div class="description"></div>').text(metadata['item' + i]);
+    var priceDiv = $('<div class="price"></div>').text(metadata['price' + i] + ' $');
+    itemDiv.append(descriptionDiv);
+    itemDiv.append(priceDiv);
+    $('.items').append(itemDiv);
+  }
+  let totalCost = 0;
+  for (var i = 1; metadata['price' + i]; i++) {
+    totalCost += parseFloat(metadata['price' + i]);
+  }
+
+  $('.sub-total').text('Sub Total: ' + totalCost.toFixed(2) + ' $');
+  $('.tax-amount').text('Tax: ' + metadata.tax_amount + ' $');
+  $('.grand-total').text('Total: ' + metadata.total_after_tax + ' $');
+}
+function display(bool) {
+  if (bool) {
+    $('.Receipt').fadeIn(350);
+  } else {
+    $('.Receipt').fadeOut(350);
+  }
+}
+
+$(function () {
+  display(false);
+
+  window.addEventListener('keyup', function (event) {
+    if (event.key === 'Escape') {
+      $.post(`https://${GetParentResourceName()}/closeUI`);
+      return;
+    }
+  });
+});
+
+window.addEventListener('message', (event) => {
+  const data = event.data;
+
+  if (data.type === 'display') {
+    if (data.enable) {
+      document.getElementById('container').style.display = 'block';
     } else {
-        $('.Receipt').fadeOut(350);
+      document.getElementById('container').style.display = 'none';
     }
   }
-  
-  $(function () {
-    display(false);
-  
-    window.addEventListener('keyup', function (event) {
-        if (event.key === 'Escape') {
-            console.log("Closing UI");
-            window.postMessage({ type: 'closeUI' });
-            return;
-        }
-    });
-  });
-  
-  window.addEventListener('message', (event) => {
-    const data = event.data;
-  
-    if (data.type === 'display') {
-        if (data.enable) {
-            document.getElementById('container').style.display = 'block';
-        } else {
-            document.getElementById('container').style.display = 'none';
-        }
-    }
-  });
+});
